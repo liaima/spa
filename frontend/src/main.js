cb79eb73954962fa20c69c1f33d54ee8712ab8dc
Manuel Lopez Jove <liaimasoft@gmail.com>
2022-06-14 17:12:48 -0300

    Add Axios

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import custom_axios from '@/plugins/axios';
+import VueAxios from 'vue-axios';
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(VueAxios, custom_axios)
   .mount('#app')
